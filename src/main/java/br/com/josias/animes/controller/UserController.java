195e13f46aa70a59eb66fba6e85651716bd827f5
Josias Junior <74216703+Jvnyor@users.noreply.github.com>
2022-01-07 15:00:51 -0300

    Update UserController.java

--- src/main/java/br/com/josias/animes/controller/UserController.java
+++ src/main/java/br/com/josias/animes/controller/UserController.java
@@ -20,7 +20,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @Tag(name="crud-users")
-@RequestMapping("/api/users")
+@RequestMapping("/api/users/admin")
 public class UserController {
 
 	@Autowired
